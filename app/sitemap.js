6edcc015bc37e32b597d2a8c929bb8d706e243aa
Bengkulu-travel <insulasiatapklaten@gmail.com>
2026-03-27 20:50:41 +0700

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.insulasiatap.biz.id',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.insulasiatap.biz.id/jogja',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+  ]
+}
