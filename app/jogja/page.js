a27318a0be153ce659c4e3c3aa9586e1e1ef6271
Bengkulu-travel <insulasiatapklaten@gmail.com>
2026-04-07 18:51:24 +0700

    Update page.js

--- app/jogja/page.js
+++ app/jogja/page.js
@@ -1,27 +1,29 @@
 import Navbar from '../../components/Navbar'
 import HeroJogja from '../../components/HeroJogja'
 import Products from '../../components/Products'
+import Kalkulator from '../../components/Kalkulator'
 import Benefits from '../../components/Benefits'
 import Gallery from '../../components/Gallery'
 import Location from '../../components/Location'
 import Footer from '../../components/Footer'
 import WhatsAppFloat from '../../components/WhatsAppFloat'
 
-
 export const metadata = {
   title: 'Insulasi Atap Yogyakarta | Solusi Nyaman & Hemat Energi',
   description: 'Toko insulasi atap terpercaya di Yogyakarta. Woven Foil, Bubble Foil, Aluminium Foam. Konsultasi gratis, harga terbaik, pengiriman area Jogja, Sleman, Bantul dan sekitarnya.',
-  keywords: 'insulasi atap jogja, insulasi atap yogyakarta, peredam panas atap yogyakarta, woven foil jogja, bubble foil yogyakarta',
+  keywords: 'insulasi atap jogja, insulasi atap yogyakarta, peredam panas atap yogyakarta',
   alternates: {
     canonical: 'https://www.insulasiatap.biz.id/jogja',
   },
 }
+
 export default function JogjaPage() {
   return (
     <main>
       <Navbar />
       <HeroJogja />
       <Products />
+      <Kalkulator />
       <Benefits />
       <Gallery />
       <Location branchId="jogja" />
