4b0352c40e6aa51ccb21786c63138e8c6d0dbb15
Bengkulu-travel <insulasiatapklaten@gmail.com>
2026-04-10 21:56:14 +0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   images: {
     formats: ['image/webp', 'image/avif'],
+    deviceSizes: [390, 640, 750, 828, 1080, 1200],
+    imageSizes: [48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 31536000,
   },
+  compress: true,
+  poweredByHeader: false,
 }
+
 module.exports = nextConfig
